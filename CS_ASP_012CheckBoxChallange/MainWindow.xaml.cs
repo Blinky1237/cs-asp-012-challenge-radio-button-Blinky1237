8bba2027646fbbac3be234f76d0e68b674581bd7
Blinky1237 <22arprecht@macon.k12.mo.us>
2020-09-30 11:06:38 -0500

    two

--- CS_ASP_012CheckBoxChallange/MainWindow.xaml.cs
+++ CS_ASP_012CheckBoxChallange/MainWindow.xaml.cs
@@ -24,5 +24,35 @@ namespace CS_ASP_012CheckBoxChallange
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (pencilRadioButton.IsChecked == true)
+            {
+                labelAnswer.Content = "Pencil, Very reliable!";
+            }
+            else if (penRadioButton.IsChecked == true)
+            {
+                labelAnswer.Content = "Pen, nice choice!";
+            }
+            else if (phoneRadioButton.IsChecked == true)
+            {
+                labelAnswer.Content = "Phone, must be from the future.";
+            }
+            else if (padRadioButton.IsChecked == true)
+            {
+                labelAnswer.Content = "Pad, Welcome to 2020!";
+            } 
+
+
+
+
+
+
+
+
+
+
+        }
     }
 }
